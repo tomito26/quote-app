281985d3a70823fe2493b52d162027c312298158
tomito26 <tommybwah@gmail.com>
2021-09-25 00:00:53 +0300

    styled the blockquote

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,9 +2,9 @@ const Quote = ({ quoteItem }) =>{
     console.log(quoteItem)
     return(
         <blockquote>
-            {/* <i class="fas fa-quote-left"></i> */}
+            <i class="fas fa-quote-left quote-1"></i>
             <p>{quoteItem.quote}</p>
-            {/* <i class="fas fa-quote-right"></i> */}
+            <i class="fas fa-quote-right quote-2"></i>
             <footer>{quoteItem.author}</footer>
         </blockquote>
     )
