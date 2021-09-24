4e4344aa55efbfaa3b7318134c65bf499d6b0e1c
tomito26 <tommybwah@gmail.com>
2021-09-25 01:01:33 +0300

    added form and text area for adding quotes, author and publisher

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -1,14 +1,23 @@
 const QuoteForm = () =>{
     return(
-    <div>
-        <form>
-            <div className="form-group">
-                <textarea name="" id="" cols="30" rows="10" placeholder="Enter Quote..."></textarea>
-            </div>
-            <div className="form-group">
-                
-            </div>
-        </form>
-    </div>
+    
+    <form>
+        <div className="form-group">
+            <label htmlFor="quote"></label>
+            <textarea name="quote" id="quote" cols="30" rows="10" placeholder="Enter Quote..."></textarea>
+        </div>
+        <div className="form-group">
+            <label htmlFor="author">Author</label>
+            <input type="text" className="form-control" id="author" name="author" />
+        </div>
+        <div className="form-group">
+            <label htmlFor="publisher">Publisher</label>
+            <input type="text" className="form-control" id="publisher" name="publisher" />
+        </div>
+        
+    </form>
+
     );
 };
+
+export default QuoteForm
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,9 +1,14 @@
 import Quote from "./Quote";
+import QuoteForm from "./QuoteForm";
+
 const Quotes = ({ quotes }) =>{
     console.log(quotes)
     return(
-    <div className="container">
-        {quotes.map((quoteItem,index)=> <Quote key={index} quoteItem={quoteItem} />)}
+    <div className="display">
+        <div className="container">
+            {quotes.map((quoteItem,index)=> <Quote key={index} quoteItem={quoteItem} />)}
+        </div>
+        <QuoteForm />
     </div>
     );
 }
